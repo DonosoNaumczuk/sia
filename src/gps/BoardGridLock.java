86e641729fde64ad11a9c15c6787f578a2b02ae5
DonosoNaumczuk <adonoso@itba.edu.ar>
2019-03-20 16:44:53 -0300

    gitignore .idea

--- src/gps/BoardGridLock.java
+++ src/gps/BoardGridLock.java
@@ -23,15 +23,24 @@ public class BoardGridLock {
      * Constructor to initialize the first gridlock board with a hardcoded
      * the configuration, given at level 1 of http://www.gtds.net/Gridlock/
      */
-    public BoardGridLock() {
-        board = new int[][] {{ 1,  1, -1, -1, -1,  7},
-                             { 2, -1, -1,  5, -1,  7},
-                             { 2,  0,  0,  5, -1,  7},
-                             { 2, -1, -1,  5, -1, -1},
-                             { 3, -1, -1, -1,  6,  6},
-                             { 3, -1,  4,  4,  4, -1}};
+    public BoardGridLock() {// 0   1   2   3   4   5 (< Y)
+        board = new int[][] {{ 1,  1, -1, -1, -1,  7},  // 0
+                             { 2, -1, -1,  5, -1,  7},  // 1
+                             { 2,  0,  0,  5, -1,  7},  // 2
+                             { 2, -1, -1,  5, -1, -1},  // 3
+                             { 3, -1, -1, -1,  6,  6},  // 4
+                             { 3, -1,  4,  4,  4, -1}}; // 5 (^ X)
 
         exit = new Point(5, 2);
+        goalBlock = new BlockGridLock(0, new Point(2, 1), new Point(2, 2));
+        blocks.add(goalBlock);
+        blocks.add(new BlockGridLock(1, new Point(0, 0), new Point(0, 1)));
+        blocks.add(new BlockGridLock(2, new Point(1, 0), new Point(3, 0)));
+        blocks.add(new BlockGridLock(3, new Point(4, 0), new Point(5, 0)));
+        blocks.add(new BlockGridLock(4, new Point(5, 2), new Point(5, 4)));
+        blocks.add(new BlockGridLock(5, new Point(1, 3), new Point(3, 3)));
+        blocks.add(new BlockGridLock(6, new Point(4, 4), new Point(4, 5)));
+        blocks.add(new BlockGridLock(7, new Point(0, ), new Point(2, )));
     }
 
     public BlockGridLock getGoalBlock() {
