0e9252ed0a94f6714ee15a2167429f0d9c061a01
Alan Donoso Naumczuk <donosonaumczuk@gmail.com>
2019-05-03 18:47:14 -0300

    Comments added describing global variables in neuralNetwork.m

--- TP2/neuralNetwork.m
+++ TP2/neuralNetwork.m
@@ -1,23 +1,23 @@
-global counter;
-global learningFactor;
-global maxError;
-global epoch;
-global isBatch;
-global beta;
-global gamma;
-global g;
-global gD;
-global betaLast;
-global gammaLast;
-global gLast;
-global gDLast;
-global dataLearning;
-global dataTest;
-global N;
-global NF;
-global weigths;
-global weigthsStart;
-global error;
+global counter;			# Index for graphics, independendent variable
+global learningFactor;	# Learning factor
+global maxError;		# Epsilon, target error to reach. All errors have to be below this value
+global epoch;			# Epoch quantity
+global isBatch;			# 1 for batch, 0 for incremental
+global beta;			#
+global gamma;			#
+global g;				# Activation function for hidden layers
+global gD;				# Activation function Derivative for hidden layers
+global betaLast;		#
+global gammaLast;		#
+global gLast;			# Activation function for last layer
+global gDLast;			# Activation function Derivative for last layer
+global dataLearning;	# Matrix with learning data for training the neural network
+global dataTest;		# Matrix with test data for calculating neural network output errors
+global N;				# Array with quantity of neurons per layer
+global NF;				# Normalizer function to avoid saturation
+global weigths;			# Array with weights
+global weigthsStart;	# Array with initial weights, in case we need to reproduce the last execution
+global error;			# TODO: we must have testErrors and learningErrors
 
 initConfiguration();
 learnNeuralNetwork();
