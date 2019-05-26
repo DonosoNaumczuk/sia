48939b5a0b59c2ef3cd5ed21b1e502b7168221ea
Alan Donoso Naumczuk <donosonaumczuk@gmail.com>
2019-05-26 16:10:51 -0300

    Set GeneticAlgorithmSolver as main class to run

--- TP3/solver/src/main/java/ar/edu/itba/sia/GeneticAlgorithmSolver.java
+++ TP3/solver/src/main/java/ar/edu/itba/sia/GeneticAlgorithmSolver.java
@@ -1,9 +1,12 @@
 package ar.edu.itba.sia;
 
 public class GeneticAlgorithmSolver {
-    //TODO: remove, just testing if can see engine module from solver module
-    GeneticAlgorithmEngine engine = new GeneticAlgorithmEngine();
 
-    //TODO: remove, just testing if can see problem module from here
-    WarriorChromosome warrior = new WarriorChromosome();
+    public static void main(String args[]) {
+        //TODO: remove, just testing if can see engine module from solver module
+        GeneticAlgorithmEngine engine = new GeneticAlgorithmEngine();
+
+        //TODO: remove, just testing if can see problem module from here
+        WarriorChromosome warrior = new WarriorChromosome();
+    }
 }
