6eac1964394aad20d202772e491a3eae121cb418
Lucas Sanz Gorostiaga <lucas.sg.07@outlook.com>
2019-05-26 17:12:49 -0300

    Deterministic tournament selection

---
+++ TP3/engine/src/main/java/ar/edu/itba/sia/DeterministicTournamentSelection.java
@@ -0,0 +1,29 @@
+package ar.edu.itba.sia;
+
+import java.util.*;
+
+public class DeterministicTournamentSelection extends TournamentSelection {
+    
+    public DeterministicTournamentSelection(final ArrayList<Individual> individuals, final int k) {
+        this.individuals = individuals;
+        m                = calculateM();
+        this.k           = k;
+    }
+
+    public ArrayList<Individual> getSelection() {
+        ArrayList<Individual> selectedList = new ArrayList<>();
+        Individual fittestInRandomSet;
+
+        for (int i = 0; i < k; i++) {
+            fittestInRandomSet = getFittestIndividualFromRandomSubset();
+            selectedList.add(fittestInRandomSet);
+        }
+
+        return selectedList;
+    }
+
+    private int calculateM() {
+        int max = 3, min = 2; // Specified in the genetic algorithms class .pdf
+        return min + rnd.nextInt((max - min) + 1); // Added 1 because nextInt's bound is exclusive
+    }
+}
