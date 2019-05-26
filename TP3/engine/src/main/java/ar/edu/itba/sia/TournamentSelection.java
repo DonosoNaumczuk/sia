cc72105c0a864d2911650d4d7c1c7dcd7461b4b6
Lucas Sanz Gorostiaga <lucas.sg.07@outlook.com>
2019-05-26 17:12:23 -0300

    Abstract tournament selection

---
+++ TP3/engine/src/main/java/ar/edu/itba/sia/TournamentSelection.java
@@ -0,0 +1,36 @@
+package ar.edu.itba.sia;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+abstract class TournamentSelection extends SelectionMethod {
+    ArrayList<Individual> individuals;
+    int m; // Amount of randomly chosen individuals
+    int k; // Iterations
+    Random rnd = new Random(); // Random instance helper
+
+    Individual getFittestIndividualFromRandomSubset() {
+        SortedSet<Individual> randoms = createRandomSubset();
+        return randoms.last();
+    }
+
+    Individual getLeastFitIndividualFromRandomSubset() {
+        SortedSet<Individual> randoms = createRandomSubset();
+        return randoms.first();
+    }
+
+    private SortedSet<Individual> createRandomSubset() {
+        Individual randomIndividual;
+        SortedSet<Individual> randoms = new TreeSet<>();
+        Random rnd                    = new Random();
+
+        for (int j = 0; j < m; j++) {
+            randomIndividual = individuals.get(rnd.nextInt(individuals.size()));
+            while (randoms.add(randomIndividual));
+        }
+
+        return randoms;
+    }
+}
