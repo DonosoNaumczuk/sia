3caba62f66913163416d354fb665593ba710ce11
Alan Donoso Naumczuk <donosonaumczuk@gmail.com>
2019-05-28 17:44:01 -0300

    remove unused line

--- TP3/solver/src/main/java/ar/edu/itba/sia/GeneticAlgorithmSolver.java
+++ TP3/solver/src/main/java/ar/edu/itba/sia/GeneticAlgorithmSolver.java
@@ -5,8 +5,5 @@ public class GeneticAlgorithmSolver {
     public static void main(String args[]) {
         //TODO: remove, just testing if can see engine module from solver module
         GeneticAlgorithmEngine engine = new GeneticAlgorithmEngine();
-
-        //TODO: remove, just testing if can see problem module from here
-        WarriorChromosome warrior = new WarriorChromosome();
     }
 }
